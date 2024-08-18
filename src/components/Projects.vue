ab549070c176cb34d0b97d4db207eee8cc7866ef
Sega <hpsergeyy@yandex.ru>
2024-08-18 19:40:52 +0200

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,30 @@
 <template>
-
   <NavigateHeader></NavigateHeader>
 
+  <v-row>
+    <v-col cols="2">
+      <v-card
+        class="mx-auto"
+        max-width="300"
+      >
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :value="item"
+            color="primary"
+            variant="plain"
+          >
+            <template v-slot:prepend>
+              <v-img style="width: 30px; height: 30px;" :inline=true :src="item.image"/>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -9,6 +32,19 @@ import NavigateHeader from "@/components/common/NavigateHeader.vue";
 
 export default {
   components: {NavigateHeader},
-  data: () => ({review: '30%'}),
+  data: () => ({
+    items: [
+      {text: 'Мой проект', image: '/assets/images/logo.svg'},
+      {text: 'Сценарии', image: '/assets/images/logo.svg'},
+      {text: 'Боты', image: '/assets/images/logo.svg'},
+      {text: 'Бронирования', image: '/assets/images/logo.svg'},
+      {text: 'Заявки и заказы', image: '/assets/images/logo.svg'},
+      {text: 'Товары и услуги', image: '/assets/images/logo.svg'},
+      {text: 'Скидки и промокоды', image: '/assets/images/logo.svg'},
+      {text: 'Доставка', image: '/assets/images/logo.svg'},
+      {text: 'Интеграции', image: '/assets/images/logo.svg'},
+      {text: 'Настройки проекта', image: '/assets/images/logo.svg'},
+    ],
+  }),
 }
 </script>
