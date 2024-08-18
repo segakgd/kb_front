3b403c90350c9170cbc488808f2669afbceee545
Sega <hpsergeyy@yandex.ru>
2024-08-18 20:03:32 +0200

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -16,7 +16,7 @@
             variant="plain"
           >
             <template v-slot:prepend>
-              <v-img style="width: 30px; height: 30px;" :inline=true :src="item.image"/>
+              <v-img style="width: 30px; height: 30px; margin-right: 10px;" :inline=true :src="item.image"/>
             </template>
 
             <v-list-item-title v-text="item.text"></v-list-item-title>
@@ -30,21 +30,39 @@
 <script lang="ts">
 import NavigateHeader from "@/components/common/NavigateHeader.vue";
 
+// Используем import.meta.glob для загрузки всех изображений
+const images = import.meta.glob('@/assets/images/*.svg');
+
 export default {
   components: {NavigateHeader},
-  data: () => ({
-    items: [
-      {text: 'Мой проект', image: '/assets/images/logo.svg'},
-      {text: 'Сценарии', image: '/assets/images/logo.svg'},
-      {text: 'Боты', image: '/assets/images/logo.svg'},
-      {text: 'Бронирования', image: '/assets/images/logo.svg'},
-      {text: 'Заявки и заказы', image: '/assets/images/logo.svg'},
-      {text: 'Товары и услуги', image: '/assets/images/logo.svg'},
-      {text: 'Скидки и промокоды', image: '/assets/images/logo.svg'},
-      {text: 'Доставка', image: '/assets/images/logo.svg'},
-      {text: 'Интеграции', image: '/assets/images/logo.svg'},
-      {text: 'Настройки проекта', image: '/assets/images/logo.svg'},
-    ],
-  }),
-}
+  data() {
+    return {
+      items: [
+        {text: 'Мой проект', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Сценарии', image: images['/src/assets/images/navigate/scenarios/bold.svg']},
+        {text: 'Боты', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Сценарии', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Боты', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Бронирования', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Заявки и заказы', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Товары и услуги', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Скидки и промокоды', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Доставка', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Интеграции', image: images['/src/assets/images/navigate/bot/bold.svg']},
+        {text: 'Настройки проекта', image: images['/src/assets/images/navigate/setting/bold.svg']},
+      ],
+    };
+  },
+  methods: {
+    async loadImages() {
+      for (const key in this.items) {
+        const image = await this.items[key].image();
+        this.items[key].image = image.default;
+      }
+    },
+  },
+  mounted() {
+    this.loadImages();
+  },
+};
 </script>
