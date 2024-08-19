179d7351194725f4d899d786920d66e789e230ea
Sega <hpsergeyy@yandex.ru>
2024-08-19 23:30:58 +0200

    +

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,18 @@
 <template>
+  <NavigateHeader></NavigateHeader>
 
-  <v-btn variant="plain" href="/projects">Проекты</v-btn>
+  <v-row>
+    <v-col cols="2">
+      <NavigateLeft/>
+    </v-col>
+    <v-col cols="8">
+      <v-btn variant="plain" href="/projects">Проекты</v-btn>
+    </v-col>
+  </v-row>
 
 </template>
 
 <script setup lang="ts">
-  //
+import NavigateHeader from "@/components/common/NavigateHeader.vue";
+import NavigateLeft from "@/components/common/NavigateLeft.vue";
 </script>
