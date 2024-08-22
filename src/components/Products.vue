c50b2a938ac9bd7c099137da5f7a9d517998f625
Sega <hpsergeyy@yandex.ru>
2024-08-23 00:37:25 +0200

    Update Products.vue

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,29 +2,9 @@
   <NavigateHeader></NavigateHeader>
 
   <v-row style="height: 100%;">
-    <v-col cols="2">
-      <NavigateLeft/>
-    </v-col>
     <v-col cols="10">
       <v-container fluid>
-        <v-row style="margin-bottom: 50px;">
-          <v-col cols="8">
-          </v-col>
 
-          <v-col cols="4" class="d-flex">
-            <v-pagination
-              :length="3"
-              density="compact"
-              color="#9b61d8"
-              style="width: 100%;"
-            ></v-pagination>
-
-            <v-btn variant="flat" class="main-btn">
-              Добавить
-            </v-btn>
-          </v-col>
-        </v-row>
-        <h1>Продукты</h1>
 
       </v-container>
     </v-col>
