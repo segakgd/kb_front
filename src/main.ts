063b584da9384b325274ccbcef0b848e4cf51918
Sega <hpsergeyy@yandex.ru>
2024-08-25 21:49:39 +0200

    added check on 401

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
-/**
- * navigate.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 // Plugins
-import { registerPlugins } from '@/plugins'
+import {registerPlugins} from '@/plugins'
 
 // Components
 import App from './App.vue'
 import store from './store';
+import router from './router';
 
 // Composables
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import axios from "axios";
 
+// Перехватчик запросов
 axios.interceptors.request.use(config => {
   const tokenFromLocalStorage = localStorage.getItem('authToken');
 
@@ -27,11 +23,31 @@ axios.interceptors.request.use(config => {
   return Promise.reject(error);
 });
 
+// Перехватчик ответов для обработки 401 ошибки
+axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response && error.response.status === 401) {
+    localStorage.removeItem('authToken');
+
+    // Используем router напрямую
+    router.push({name: 'Login'});
+
+    // Можно также добавить уведомление через store, если нужно
+    store.commit('setSnackbar', {
+      message: 'Авторизация истекла, пожалуйста, войдите заново.',
+      color: 'error'
+    });
+  }
+
+  return Promise.reject(error);
+});
 
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.use(store);
+app.use(router);
 
 app.mount('#app')
