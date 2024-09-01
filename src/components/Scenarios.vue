1e0da4869ad6cd8ee95949540a34360a3891b350
Sega <hpsergeyy@yandex.ru>
2024-09-01 16:05:38 +0200

    clear and move styles

--- src/components/Scenarios.vue
+++ src/components/Scenarios.vue
@@ -15,23 +15,23 @@
         <v-row>
           <v-col cols="4" v-for="(scenario, index) in scenarios" :key="index">
             <a
-              href="/bot/"
-              class="bot-item"
+              :href="`/scenario/${scenario.id}`"
+              class="box-block-item"
             >
               <div style="width: 100%;">
                 <div style="display: flex; justify-content: end; align-items: center; width: 100%; margin-bottom: 10px;">
-                  <span class="bot-status--active" v-if="scenario.active">Используется</span>
-                  <span class="bot-status--blocked" v-else>Не используется</span>
+                  <span class="status--active" v-if="scenario.active">Используется</span>
+                  <span class="status--blocked" v-else>Не используется</span>
                 </div>
 
-                <div class="bot-item--name" style="display: flex; align-items: center;">
+                <div class="box-block-item--name" style="display: flex; align-items: center;">
                   <span>{{ scenario.name }}</span>
                 </div>
-                <div class="bot-item--field">Шаблон:
+                <div class="box-block-item--field">Шаблон:
                   <span>{{ scenario.template }}</span>
                 </div>
-                <div class="bot-item--field mt-3">Последнее изменение: <span>{{ scenario.updatedAt }}</span></div>
-                <div class="bot-item--field">Создан: <span>{{ scenario.createdAt }}</span></div>
+                <div class="box-block-item--field mt-3">Последнее изменение: <span>{{ scenario.updatedAt }}</span></div>
+                <div class="box-block-item--field">Создан: <span>{{ scenario.createdAt }}</span></div>
               </div>
             </a>
           </v-col>
@@ -54,7 +54,7 @@
     <FilterForm
       v-else
       btn-name="Добавить сценарий"
-      :uri=`http://0.0.0.0/api/admin/project/${this.projectId}/scenario-template/`
+      :uri="`http://0.0.0.0/api/admin/project/${this.projectId}/scenario-template/`"
       :httpMethod=HttpMethodEnum.Get
       :fields="fields"
       @btnClick="triggerDialog"
@@ -149,50 +149,6 @@ export default {
 
       loader: false,
       filterLoaded: false,
-
-
-      bots: [
-        {
-          id: 1,
-          name: 'Название сценария 1',
-          active: true,
-          template: 'Магазин',
-          createdAt: '2024-10-23',
-          updatedAt: '2024-10-23',
-        },
-        {
-          id: 2,
-          name: 'Самый топовый сценарий',
-          active: true,
-          template: 'Пользовательский',
-          createdAt: '2024-10-23',
-          updatedAt: '2024-10-23',
-        },
-        {
-          id: 3,
-          name: 'Название сценария 3',
-          active: false,
-          template: 'Курсы',
-          createdAt: '2024-10-23',
-          updatedAt: '2024-10-23',
-        },
-        {
-          id: 4,
-          name: 'Название сценария 4',
-          active: false,
-          template: 'Пользовательский',
-          createdAt: '2024-10-23',
-          updatedAt: '2024-10-23',
-        },
-        {
-          id: 5,
-          name: 'Мой старый сценарий',
-          active: false,
-          template: 'Пользовательский',
-          createdAt: '2024-10-23',
-          updatedAt: '2024-10-23',
-        },
-      ],
     };
   },
   mounted() {
@@ -274,60 +230,4 @@ export default {
 </script>
 
 <style scoped>
-.bot-item {
-  box-shadow: 0 0 5px 0 rgba(34, 60, 80, 0.2);
-  padding: 30px 20px 20px 20px;
-  display: flex;
-  width: 100%;
-  border-radius: 10px;
-  text-decoration: none;
-  color: #3C415E;
-  position: relative;
-  transition: 0.4s;
-  overflow: hidden;
-}
-
-.bot-item:hover {
-  box-shadow: 0 0 5px 2px rgba(34, 60, 80, 0.2);
-}
-
-.bot-item .bot-status--active, .bot-item .bot-status--blocked {
-  padding: 1px 10px;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 12px;
-  line-height: 24px;
-  display: flex;
-  align-items: center;
-
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-}
-
-.bot-item .bot-status--active {
-  color: #067306;
-  background: #D9FABF;
-}
-
-.bot-item .bot-status--blocked {
-  color: #9E0038;
-  background: #FFD4F5;
-}
-
-.bot-item--name {
-  padding-bottom: 10px;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 27px;
-  color: #3C415E;
-}
-
-.bot-item--field {
-  color: #3C415E;
-  font-weight: 300;
-  font-size: 15px;
-}
 </style>
